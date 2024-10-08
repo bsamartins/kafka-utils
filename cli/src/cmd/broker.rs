ecf7a73df468b5e505c013548b7eef38d1a3c93d
Bernardo Martins <bernardo.martins@utmost.co>
2024-10-08 19:36:14 +0100

    Print broker table

--- cli/src/cmd/broker.rs
+++ cli/src/cmd/broker.rs
@@ -1,14 +1,18 @@
-use std::time::Duration;
+use crate::cmd::table;
 use clap::{Args, Subcommand};
 use common::kafka;
-use common::kafka::{client::IamClientContext};
+use common::kafka::client::IamClientContext;
+use common::kafka::types::ListBrokerEntry;
 use rdkafka::ClientConfig;
+use std::borrow::Cow;
+use std::time::Duration;
+use tabled::Tabled;
 
 pub fn list_brokers_cmd(client_config: ClientConfig, context: IamClientContext, timeout: Duration) {
-    let brokers = kafka::broker::list_brokers(client_config, context, timeout);
-    brokers.iter().for_each(|broker| {
-        println!("[{}] {}:{}", broker.id, broker.host, broker.port)
-    });
+    let brokers = kafka::broker::list_brokers(client_config, context, timeout)
+        .iter().map(|e| ListBrokerTable(e.clone()))
+        .collect();
+    println!("{}", table::create(brokers))
 }
 
 #[derive(Debug, Args)]
@@ -22,4 +26,26 @@ pub struct ClusterArgs {
 #[derive(Debug, Subcommand)]
 pub enum ClusterCommands {
     Brokers,
+}
+
+struct ListBrokerTable(ListBrokerEntry);
+
+impl Tabled for ListBrokerTable {
+    const LENGTH: usize = 3;
+
+    fn fields(&self) -> Vec<Cow<'_, str>> {
+        vec![
+            self.0.id.to_string().into(),
+            self.0.host.as_str().into(),
+            self.0.port.to_string().into(),
+        ]
+    }
+
+    fn headers() -> Vec<Cow<'static, str>> {
+        vec![
+            "ID".into(),
+            "Host".into(),
+            "Port".into(),
+        ]
+    }
 }
