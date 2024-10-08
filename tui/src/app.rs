588027446aa8edab4f672604fa47cc7ba91e8244
Bernardo Martins <bernardo.martins@utmost.co>
2024-10-08 23:25:43 +0100

    Add layout

--- tui/src/app.rs
+++ tui/src/app.rs
@@ -1,13 +1,11 @@
-use ratatui::{DefaultTerminal, Frame};
+use color_eyre::eyre::{bail, WrapErr};
 use crossterm::event;
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
-use color_eyre::eyre::{bail, WrapErr};
-use ratatui::prelude::{Line, Stylize, Text, Widget};
-use ratatui::layout::{Alignment, Rect};
 use ratatui::buffer::Buffer;
-use ratatui::widgets::block::{Position, Title};
-use ratatui::widgets::{Block, Paragraph};
-use ratatui::symbols::border;
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::prelude::Widget;
+use ratatui::widgets::{Block, Borders};
+use ratatui::{DefaultTerminal, Frame};
 
 #[derive(Debug, Default)]
 pub struct App {
@@ -71,32 +69,48 @@ impl App {
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .border_set(border::THICK);
+        // let title = Title::from(" Counter App Tutorial ".bold());
+        // let instructions = Title::from(Line::from(vec![
+        //     " Decrement ".into(),
+        //     "<Left>".blue().bold(),
+        //     " Increment ".into(),
+        //     "<Right>".blue().bold(),
+        //     " Quit ".into(),
+        //     "<Q> ".blue().bold(),
+        // ]));
+        // let block = Block::bordered()
+        //     .title(title.alignment(Alignment::Center))
+        //     .title(
+        //         instructions
+        //             .alignment(Alignment::Center)
+        //             .position(Position::Bottom),
+        //     )
+        //     .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
+        // let counter_text = Text::from(vec![Line::from(vec![
+        //     "Value: ".into(),
+        //     self.counter.to_string().yellow(),
+        // ])]);
 
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
+        // Paragraph::new(counter_text)
+        //     .centered()
+        //     .block(block)
+        //     .render(area, buf);
+
+        let vertical = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Min(1),
+        ]);
+
+        let [input_area, main_area] = vertical.areas(area);
+
+        Block::default()
+            .title("Input")
+            .borders(Borders::ALL)
+            .render(input_area, buf);
+        Block::default()
+            .title("Main")
+            .borders(Borders::ALL)
+            .render(main_area, buf);
     }
 }
